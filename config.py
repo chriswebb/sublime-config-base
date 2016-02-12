99a61ea92a6cd1dd1912f0faf721c41b65fd1531
Chris Webb <chriswebb@users.noreply.github.com>
2016-02-11 23:51:04 -0600

    Fixed imports and added license

--- config.py
+++ config.py
@@ -1,3 +1,33 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2016 Chris Webb
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from sublime import save_settings, load_settings, status_message, active_window, set_timeout
+from sublime_plugin import TextCommand, WindowCommand, EventListener
+from collections import MutableMapping 
+from abc import ABCMeta
+
+def set_status(msg):
+    set_timeout(lambda:status_message(msg))
+
 class ConfigBaseTextCommand(TextCommand, metaclass=ABCMeta):  
 
     __settings = None
@@ -32,6 +62,13 @@ class ConfigBaseWindowCommand(WindowCommand, metaclass=ABCMeta):
     @settings.setter
     def function(self, *args, **kwargs):
         self.__settings.update(dict(*args, **kwargs))
+        
+        
+class ConfigSettingsEventListener(EventListener):  
+    def post_window_command(window, command_name, args):
+        if command_name == 'close_window':
+            ConfigSettings.window_closed(window)
+
 
 class ConfigSettings(MutableMapping):
     __settings_name = 'My Plugin.sublime-settings'  # Name of the settings file for the current plugin.
